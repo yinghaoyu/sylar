a16e4e01ef50d33b7683eb9970052161d1f13039
rain <20520@126.com>
2023-05-15 21:48:42 +0800

    ⚡ 优化SmtpClient错误显示,增加StringUtil方法

--- sylar/util.h
+++ sylar/util.h
@@ -184,6 +184,16 @@ class StringUtil {
                                bool space_as_plus = true);
   static std::string UrlDecode(const std::string& str,
                                bool space_as_plus = true);
+
+  static std::string Trim(const std::string& str,
+                          const std::string& delimit = " \t\r\n");
+  static std::string TrimLeft(const std::string& str,
+                              const std::string& delimit = " \t\r\n");
+  static std::string TrimRight(const std::string& str,
+                               const std::string& delimit = " \t\r\n");
+
+  static std::string WStringToString(const std::wstring& ws);
+  static std::wstring StringToWString(const std::string& s);
 };
 
 }  // namespace sylar

12d2bc51d6e8283282adda5399cc7370de7f2ca2
rain <20520@126.com>
2023-05-17 16:06:15 +0800

    🐛 修复test_fiber

--- tests/test_fiber.cc
+++ tests/test_fiber.cc
@@ -3,10 +3,11 @@
 sylar::Logger::ptr g_logger = SYLAR_LOG_ROOT();
 
 void run_in_fiber() {
+  sylar::Fiber::ptr self = sylar::Fiber::GetThis();
   SYLAR_LOG_INFO(g_logger) << "run_in_fiber begin";
-  sylar::Fiber::YieldToHold();
+  self->back();
   SYLAR_LOG_INFO(g_logger) << "run_in_fiber end";
-  sylar::Fiber::YieldToHold();
+  self->back();
 }
 
 void test_fiber() {
@@ -14,12 +15,12 @@ void test_fiber() {
   {
     sylar::Fiber::GetThis();
     SYLAR_LOG_INFO(g_logger) << "main begin";
-    sylar::Fiber::ptr fiber(new sylar::Fiber(run_in_fiber));
-    fiber->swapIn();
+    sylar::Fiber::ptr fiber(new sylar::Fiber(run_in_fiber, 0, true));
+    fiber->call();
     SYLAR_LOG_INFO(g_logger) << "main after swapIn";
-    fiber->swapIn();
+    fiber->call();
     SYLAR_LOG_INFO(g_logger) << "main after end";
-    fiber->swapIn();
+    fiber->call();
   }
   SYLAR_LOG_INFO(g_logger) << "main after end2";
 }
@@ -28,7 +29,7 @@ int main(int argc, char** argv) {
   sylar::Thread::SetName("main");
 
   std::vector<sylar::Thread::ptr> thrs;
-  for (int i = 0; i < 3; ++i) {
+  for (int i = 0; i < 1; ++i) {
     thrs.push_back(sylar::Thread::ptr(
         new sylar::Thread(&test_fiber, "name_" + std::to_string(i))));
   }

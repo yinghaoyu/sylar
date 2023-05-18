05d790540bc0feb4d1032ca33d4c44c16a6c8216
rain <20520@126.com>
2023-05-18 14:46:37 +0800

    🐛 修复test_scheduler

--- tests/test_scheduler.cc
+++ tests/test_scheduler.cc
@@ -5,8 +5,8 @@ static sylar::Logger::ptr g_logger = SYLAR_LOG_ROOT();
 void test_fiber() {
   static int s_count = 5;
   SYLAR_LOG_INFO(g_logger) << "test in fiber s_count=" << s_count;
-
-  sleep(1);
+  // sleep 已经被 hook
+  // sleep(1);
   if (--s_count >= 0) {
     sylar::Scheduler::GetThis()->schedule(&test_fiber, sylar::GetThreadId());
   }
@@ -14,9 +14,9 @@ void test_fiber() {
 
 int main(int argc, char** argv) {
   SYLAR_LOG_INFO(g_logger) << "main";
-  sylar::Scheduler sc(3, false, "test");
+  sylar::Scheduler sc(1, false, "test");
   sc.start();
-  sleep(2);
+  // sleep(2);
   SYLAR_LOG_INFO(g_logger) << "schedule";
   sc.schedule(&test_fiber);
   sc.stop();
